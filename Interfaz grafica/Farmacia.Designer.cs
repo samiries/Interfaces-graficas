6460033b32136a6e746b81ff1ad783c638dc4537
samiries <samiries21@gmail.com>
2026-09-02 13:02:06 -0300

    farmacia terminado

--- Interfaz grafica/Farmacia.Designer.cs
+++ Interfaz grafica/Farmacia.Designer.cs
@@ -29,138 +29,227 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Farmacia));
-            lbldatos = new Label();
+            lbldatos1 = new Label();
             lbltitulo = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
-            textBox5 = new TextBox();
+            txtnombre = new TextBox();
+            txtlaboratorio = new TextBox();
+            txtcodigo = new TextBox();
+            txtprecio = new TextBox();
+            txtstock = new TextBox();
             cmbtipo = new ComboBox();
             btnagregar = new Button();
             btnmodificar = new Button();
             btnborrar = new Button();
             lst = new ListView();
+            Medicamento = new ColumnHeader();
+            Precio = new ColumnHeader();
+            Stock = new ColumnHeader();
             btnbuscar = new Button();
             txtbuscar = new TextBox();
+            lbldatos2 = new Label();
+            lbldatos3 = new Label();
+            lbldatos4 = new Label();
+            lbldatos5 = new Label();
+            lbldatos6 = new Label();
             SuspendLayout();
             // 
-            // lbldatos
+            // lbldatos1
             // 
-            lbldatos.AutoSize = true;
-            lbldatos.Font = new Font("Impact", 15.75F, FontStyle.Italic, GraphicsUnit.Point, 0);
-            lbldatos.Location = new Point(45, 42);
-            lbldatos.Name = "lbldatos";
-            lbldatos.Size = new Size(63, 26);
-            lbldatos.TabIndex = 0;
-            lbldatos.Text = "label1";
-            lbldatos.Click += label1_Click;
+            lbldatos1.AutoSize = true;
+            lbldatos1.Font = new Font("Impact", 15.75F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lbldatos1.Location = new Point(108, 50);
+            lbldatos1.Name = "lbldatos1";
+            lbldatos1.Size = new Size(133, 26);
+            lbldatos1.TabIndex = 0;
+            lbldatos1.Text = "Medicamento:";
+            lbldatos1.Click += label1_Click;
             // 
             // lbltitulo
             // 
             lbltitulo.AutoSize = true;
-            lbltitulo.Location = new Point(176, 9);
+            lbltitulo.Font = new Font("Algerian", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            lbltitulo.Location = new Point(108, 9);
             lbltitulo.Name = "lbltitulo";
-            lbltitulo.Size = new Size(112, 15);
+            lbltitulo.Size = new Size(254, 24);
             lbltitulo.TabIndex = 1;
             lbltitulo.Text = "Gestion de farmacia";
             lbltitulo.Click += lbltitulo_Click;
             // 
-            // textBox1
+            // txtnombre
             // 
-            textBox1.Location = new Point(176, 42);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 2;
+            txtnombre.Location = new Point(251, 53);
+            txtnombre.Name = "txtnombre";
+            txtnombre.Size = new Size(100, 23);
+            txtnombre.TabIndex = 2;
             // 
-            // textBox2
+            // txtlaboratorio
             // 
-            textBox2.Location = new Point(176, 71);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
-            textBox2.TabIndex = 3;
+            txtlaboratorio.Location = new Point(251, 82);
+            txtlaboratorio.Name = "txtlaboratorio";
+            txtlaboratorio.Size = new Size(100, 23);
+            txtlaboratorio.TabIndex = 3;
             // 
-            // textBox3
+            // txtcodigo
             // 
-            textBox3.Location = new Point(176, 100);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(100, 23);
-            textBox3.TabIndex = 4;
+            txtcodigo.Location = new Point(251, 111);
+            txtcodigo.Name = "txtcodigo";
+            txtcodigo.Size = new Size(100, 23);
+            txtcodigo.TabIndex = 4;
             // 
-            // textBox4
+            // txtprecio
             // 
-            textBox4.Location = new Point(176, 129);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(100, 23);
-            textBox4.TabIndex = 5;
+            txtprecio.Location = new Point(251, 140);
+            txtprecio.Name = "txtprecio";
+            txtprecio.Size = new Size(100, 23);
+            txtprecio.TabIndex = 5;
+            txtprecio.KeyPress += txtprecio_KeyPress;
             // 
-            // textBox5
+            // txtstock
             // 
-            textBox5.Location = new Point(176, 177);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(100, 23);
-            textBox5.TabIndex = 6;
+            txtstock.Location = new Point(251, 169);
+            txtstock.Name = "txtstock";
+            txtstock.Size = new Size(100, 23);
+            txtstock.TabIndex = 6;
+            txtstock.KeyPress += txtstock_KeyPress;
             // 
             // cmbtipo
             // 
             cmbtipo.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbtipo.FormattingEnabled = true;
             cmbtipo.Items.AddRange(new object[] { "Venta libre", "Receta\t\t\t" });
-            cmbtipo.Location = new Point(167, 239);
+            cmbtipo.Location = new Point(242, 234);
             cmbtipo.Name = "cmbtipo";
             cmbtipo.Size = new Size(121, 23);
             cmbtipo.TabIndex = 7;
             // 
             // btnagregar
             // 
+            btnagregar.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Italic);
             btnagregar.Location = new Point(90, 304);
             btnagregar.Name = "btnagregar";
-            btnagregar.Size = new Size(75, 23);
+            btnagregar.Size = new Size(94, 30);
             btnagregar.TabIndex = 8;
             btnagregar.Text = "Agregar";
             btnagregar.UseVisualStyleBackColor = true;
+            btnagregar.Click += btnagregar_Click;
             // 
             // btnmodificar
             // 
-            btnmodificar.Location = new Point(201, 304);
+            btnmodificar.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Italic);
+            btnmodificar.Location = new Point(208, 304);
             btnmodificar.Name = "btnmodificar";
-            btnmodificar.Size = new Size(75, 23);
+            btnmodificar.Size = new Size(94, 30);
             btnmodificar.TabIndex = 9;
             btnmodificar.Text = "Modificar";
             btnmodificar.UseVisualStyleBackColor = true;
+            btnmodificar.Click += btnmodificar_Click;
             // 
             // btnborrar
             // 
+            btnborrar.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Italic);
             btnborrar.Location = new Point(325, 304);
             btnborrar.Name = "btnborrar";
-            btnborrar.Size = new Size(75, 23);
+            btnborrar.Size = new Size(94, 30);
             btnborrar.TabIndex = 10;
             btnborrar.Text = "Borrar";
             btnborrar.UseVisualStyleBackColor = true;
+            btnborrar.Click += btnborrar_Click;
             // 
             // lst
             // 
-            lst.Location = new Point(90, 371);
+            lst.Columns.AddRange(new ColumnHeader[] { Medicamento, Precio, Stock });
+            lst.FullRowSelect = true;
+            lst.Location = new Point(63, 371);
             lst.Name = "lst";
-            lst.Size = new Size(310, 97);
+            lst.Size = new Size(398, 97);
             lst.TabIndex = 11;
             lst.UseCompatibleStateImageBehavior = false;
+            lst.View = View.Details;
+            lst.SelectedIndexChanged += lst_SelectedIndexChanged;
+            // 
+            // Medicamento
+            // 
+            Medicamento.Text = "Medicamento";
+            Medicamento.Width = 200;
+            // 
+            // Precio
+            // 
+            Precio.Text = "Precio";
+            Precio.Width = 90;
+            // 
+            // Stock
+            // 
+            Stock.Text = "Stock";
+            Stock.Width = 90;
             // 
             // btnbuscar
             // 
-            btnbuscar.Location = new Point(234, 502);
+            btnbuscar.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnbuscar.Location = new Point(234, 493);
             btnbuscar.Name = "btnbuscar";
-            btnbuscar.Size = new Size(100, 23);
-            btnbuscar.TabIndex = 12;
+            btnbuscar.Size = new Size(117, 34);
+            btnbuscar.TabIndex = 13;
             btnbuscar.Text = "Buscar medicamento";
             btnbuscar.UseVisualStyleBackColor = true;
+            btnbuscar.Click += btnbuscar_Click;
             // 
             // txtbuscar
             // 
-            txtbuscar.Location = new Point(90, 503);
+            txtbuscar.Location = new Point(90, 502);
             txtbuscar.Name = "txtbuscar";
             txtbuscar.Size = new Size(100, 23);
-            txtbuscar.TabIndex = 13;
+            txtbuscar.TabIndex = 12;
+            txtbuscar.TextChanged += txtbuscar_TextChanged;
+            // 
+            // lbldatos2
+            // 
+            lbldatos2.AutoSize = true;
+            lbldatos2.Font = new Font("Impact", 15.75F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lbldatos2.Location = new Point(108, 82);
+            lbldatos2.Name = "lbldatos2";
+            lbldatos2.Size = new Size(117, 26);
+            lbldatos2.TabIndex = 15;
+            lbldatos2.Text = "Laboratorio:";
+            // 
+            // lbldatos3
+            // 
+            lbldatos3.AutoSize = true;
+            lbldatos3.Font = new Font("Impact", 15.75F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lbldatos3.Location = new Point(108, 108);
+            lbldatos3.Name = "lbldatos3";
+            lbldatos3.Size = new Size(77, 26);
+            lbldatos3.TabIndex = 16;
+            lbldatos3.Text = "Código:";
+            // 
+            // lbldatos4
+            // 
+            lbldatos4.AutoSize = true;
+            lbldatos4.Font = new Font("Impact", 15.75F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lbldatos4.Location = new Point(108, 140);
+            lbldatos4.Name = "lbldatos4";
+            lbldatos4.Size = new Size(72, 26);
+            lbldatos4.TabIndex = 17;
+            lbldatos4.Text = "Precio:";
+            // 
+            // lbldatos5
+            // 
+            lbldatos5.AutoSize = true;
+            lbldatos5.Font = new Font("Impact", 15.75F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lbldatos5.Location = new Point(108, 169);
+            lbldatos5.Name = "lbldatos5";
+            lbldatos5.Size = new Size(64, 26);
+            lbldatos5.TabIndex = 18;
+            lbldatos5.Text = "Stock:";
+            // 
+            // lbldatos6
+            // 
+            lbldatos6.AutoSize = true;
+            lbldatos6.Font = new Font("Impact", 15.75F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lbldatos6.Location = new Point(108, 234);
+            lbldatos6.Name = "lbldatos6";
+            lbldatos6.Size = new Size(53, 26);
+            lbldatos6.TabIndex = 19;
+            lbldatos6.Text = "Tipo:";
             // 
             // Farmacia
             // 
@@ -169,6 +258,11 @@
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(556, 564);
+            Controls.Add(lbldatos6);
+            Controls.Add(lbldatos5);
+            Controls.Add(lbldatos4);
+            Controls.Add(lbldatos3);
+            Controls.Add(lbldatos2);
             Controls.Add(txtbuscar);
             Controls.Add(btnbuscar);
             Controls.Add(lst);
@@ -176,14 +270,16 @@
             Controls.Add(btnmodificar);
             Controls.Add(btnagregar);
             Controls.Add(cmbtipo);
-            Controls.Add(textBox5);
-            Controls.Add(textBox4);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
+            Controls.Add(txtstock);
+            Controls.Add(txtprecio);
+            Controls.Add(txtcodigo);
+            Controls.Add(txtlaboratorio);
+            Controls.Add(txtnombre);
             Controls.Add(lbltitulo);
-            Controls.Add(lbldatos);
+            Controls.Add(lbldatos1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Farmacia";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Farmacia";
             ResumeLayout(false);
             PerformLayout();
@@ -191,13 +287,13 @@
 
         #endregion
 
-        private Label lbldatos;
+        private Label lbldatos1;
         private Label lbltitulo;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private TextBox textBox4;
-        private TextBox textBox5;
+        private TextBox txtnombre;
+        private TextBox txtlaboratorio;
+        private TextBox txtcodigo;
+        private TextBox txtprecio;
+        private TextBox txtstock;
         private ComboBox cmbtipo;
         private Button btnagregar;
         private Button btnmodificar;
@@ -205,5 +301,13 @@
         private ListView lst;
         private Button btnbuscar;
         private TextBox txtbuscar;
+        private Label lbldatos2;
+        private Label lbldatos3;
+        private Label lbldatos4;
+        private Label lbldatos5;
+        private Label lbldatos6;
+        private ColumnHeader Medicamento;
+        private ColumnHeader Precio;
+        private ColumnHeader Stock;
     }
 }
--- Interfaz grafica/Farmacia.cs
+++ Interfaz grafica/Farmacia.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Text;
 using System.Windows.Forms;
 
@@ -13,7 +14,35 @@ namespace Interfaz_grafica
         public Farmacia()
         {
             InitializeComponent();
-            lbldatos.Text = "Medicamento: \nLaboratorio: \nCòdigo: \nPrecio: \n\nStock: \n\nTipo:  ";
+            txtnombre.TextChanged += validarbtnagregar;
+            txtlaboratorio.TextChanged += validarbtnagregar;
+            txtcodigo.TextChanged += validarbtnagregar;
+            txtprecio.TextChanged += validarbtnagregar;
+            txtstock.TextChanged += validarbtnagregar;
+            cmbtipo.SelectedIndexChanged += validarbtnagregar;
+
+
+
+
+            btnagregar.Enabled = false;
+            btnborrar.Enabled = false;
+            btnmodificar.Enabled = false;
+
+        }
+
+
+
+
+        private void validarbtnagregar(object sender, EventArgs e)
+        {
+            if (txtnombre.Text != "" && txtlaboratorio.Text != "" && txtcodigo.Text != "" && txtprecio.Text != "" && txtstock.Text != "" && cmbtipo.SelectedIndex != -1)
+            {
+                btnagregar.Enabled = true;
+            }
+            else
+            {
+                btnagregar.Enabled = false;
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -25,5 +54,115 @@ namespace Interfaz_grafica
         {
 
         }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtprecio_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != ',')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtstock_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtnombre.Text;
+            string laboratorio = txtlaboratorio.Text;
+            string codigo = txtcodigo.Text;
+            double precio = double.Parse(txtprecio.Text);
+            int stock = int.Parse(txtstock.Text);
+
+
+            ListViewItem item = new ListViewItem(nombre);
+            item.SubItems.Add(precio.ToString());
+            item.SubItems.Add(stock.ToString());
+
+            lst.Items.Add(item);
+
+            txtnombre.Clear();
+            txtlaboratorio.Clear();
+            txtcodigo.Clear();
+            txtprecio.Clear();
+            txtstock.Clear();
+            cmbtipo.SelectedIndex = -1;
+
+        }
+
+        private void btnborrar_Click(object sender, EventArgs e)
+        {
+            lst.Items.Remove(lst.SelectedItems[0]);
+        }
+
+        private void lst_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lst.SelectedItems.Count > 0)
+            {
+
+                ListViewItem item = lst.SelectedItems[0];
+
+                txtnombre.Text = item.Text;
+                txtprecio.Text = item.SubItems[1].Text;
+                txtstock.Text = item.SubItems[2].Text;
+
+
+                btnmodificar.Enabled = true;
+                btnborrar.Enabled = true;
+                btnagregar.Enabled = false;
+            }
+
+
+        }
+
+        private void btnmodificar_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = lst.SelectedItems[0];
+            item.Text = txtnombre.Text;
+            item.SubItems[1].Text = txtprecio.Text;
+            item.SubItems[2].Text = txtstock.Text;
+            lst.SelectedItems[0].Selected = false;
+
+        }
+
+        private void btnbuscar_Click(object sender, EventArgs e)
+        {
+            string busqueda = txtbuscar.Text.ToLower();
+            bool encontrado = false;
+
+            foreach (ListViewItem item in lst.Items)
+            {
+                if (item.Text.ToLower().Contains(busqueda))
+                {
+                    item.Selected = true;
+                    item.EnsureVisible();
+                    encontrado = true;
+                    break;
+                }
+            }
+
+            if (!encontrado)
+            {
+                MessageBox.Show("No se encontró el medicamento.", "AVISO");
+            }
+        }
+
+        private void txtbuscar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
+
 }
+
+
